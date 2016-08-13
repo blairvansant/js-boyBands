df8d656c1999d0bf3430b31d45d701faed1d7ee9
blairvansant <blairbot@gmail.com>
2016-08-13 11:04:09 -0500

    fin

--- boybands.js
+++ boybands.js
@@ -1,3 +1,6 @@
+//Loop through the two arrays provided (bands and vegetables) and output each element in the arrays into their corresponding HTML <div> element. 
+//Ensure that each item is in a block element (e.g. li, div, p. etc...)
+
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
@@ -11,19 +14,27 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+for (var i = 0; i < loopCount; i++) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
+  bandElement.innerHTML += "<div>" + bands[i] + "</div>";
 
 
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  veggieElement.innerHTML += "<li>" + vegetables[i] + "</li>" 
+ }
 
-}
+  
+
+
+
+
+
+
+	
